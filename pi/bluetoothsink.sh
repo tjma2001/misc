4221f38f9e3952818254b9cd103586cb3975ada1
Taariq Mullins <tjma2001@gmail.com>
2018-09-14 15:13:00 +0200

    Updated bluetooth script commands

--- pi/bluetoothsink.sh
+++ pi/bluetoothsink.sh
@@ -2,10 +2,10 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install pulseaudio-module-bluetooth bluez-tools
 
-sudo gpasswd -a xbian pulse
-sudo gpasswd -a xbian lp
+sudo gpasswd -a $USER pulse
+sudo gpasswd -a $USER lp
 sudo gpasswd -a pulse lp
-sudo gpasswd -a xbian audio
+sudo gpasswd -a $USER audio
 sudo gpasswd -a pulse audio
 
 sudo sh -c "echo 'extra-arguments = --exit-idle-time=-1 --log-target=syslog' >> /etc/pulse/client.conf"
